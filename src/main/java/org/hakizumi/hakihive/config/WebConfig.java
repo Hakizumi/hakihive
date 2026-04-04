ad23c79771057ba7e14af24bfa281ada6f6e780d
Hakizumi <Hakizumi@outlook.com>
2026-04-04 08:12:45 +0800

    fix: Registered `ServerWebSocketHandler` to `spring websocket`

--- src/main/java/org/hakizumi/hakihive/config/WebConfig.java
+++ src/main/java/org/hakizumi/hakihive/config/WebConfig.java
@@ -1,19 +1,34 @@
 package org.hakizumi.hakihive.config;
 
+import org.hakizumi.hakihive.controller.ServerWebSocketHandler;
 import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebSocketConfigurer, WebMvcConfigurer {
+    private final ServerWebSocketHandler serverWebSocketHandler;
+
+    public WebConfig(ServerWebSocketHandler serverWebSocketHandler) {
+        this.serverWebSocketHandler = serverWebSocketHandler;
+    }
+
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/backend/chat/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "OPTIONS")
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void registerWebSocketHandlers(@NonNull WebSocketHandlerRegistry registry) {
+        registry.addHandler(serverWebSocketHandler, "/backend/websocket/chat")
+                .setAllowedOriginPatterns("*");
     }
 
     @Override
